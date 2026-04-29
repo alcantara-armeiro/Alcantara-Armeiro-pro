cae8de255257f3f7fba7b8962e8a5d6f172cdec3
Alcantara Armeiro <alcantara.armeiro@gmail.com>
2026-04-28 21:57:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'alcantara-v7-1-1-premium';
-const RUNTIME_CACHE = 'alcantara-runtime-v7-1-1-premium';
+const CACHE_NAME = 'alcantara-v7-1-2-margem-pecas';
+const RUNTIME_CACHE = 'alcantara-runtime-v7-1-2-margem-pecas';
 
 const CORE_ASSETS = [
   './',
